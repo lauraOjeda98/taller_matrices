b5863ea8fd4ece3a4bf421805e11320cb1694638
lauraOjeda98 <70666704+lauraOjeda98@users.noreply.github.com>
2021-11-28 14:26:19 -0500

    Segundo punto

--- taller_matrices.py
+++ taller_matrices.py
@@ -10,6 +10,10 @@ import pandas as pd
 url = '../covid_22_noviembre.csv'
 data = pd.read_csv(url)
 
-# Número de casos de Contagiados en el País
+# 1. Número de casos de Contagiados en el País
 casos = data.shape[0]
 print(f'El número de casos de contagios en el país es de: {casos}')
+
+# 2. Número de Municipios Afectados
+municipios = data['Nombre municipio'].value_counts().shape[0]
+print(f'El número de municipios afectados es de: {municipios}')
