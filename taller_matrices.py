4740e02dc0d37d920f11aa2878f922cbfa45809e
lauraOjeda98 <70666704+lauraOjeda98@users.noreply.github.com>
2021-11-30 21:41:27 -0500

    Corregir error en archivos

--- taller_matrices.py
+++ taller_matrices.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Nov 28 13:22:55 2021
+Created on Sun Nov 28 12:07:48 2021
 
 @author: laura
 """
 
 import pandas as pd
-import matplotlib.pyplot as plt
 
 url = 'covid_22_noviembre.csv'
 data = pd.read_csv(url)
